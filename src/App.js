fd609f3930d011db5e64f485643decde2be1d949
Aamir Khan <aamirkhan94work@gmail.com>
2023-01-28 08:37:11 +0000

    add in creatives + fix date formatting

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [title, setTitle] = useState('');
   const [date, setDate] = useState('');
   const [shortDescription, setShortDescription] = useState('');
+  const [creatives, setCreatives] = useState([]);
 
   let performanceID = 14
 
@@ -18,7 +19,8 @@ function App() {
       console.log(data);
       setTitle(data.data.attributes.title);
       setShortDescription(data.data.attributes.shortDescription);
-      setDate(data.included[performanceID].attributes.date);
+      setDate(formatDate(data.included[performanceID].attributes.date));
+      getCreatives(data.included)
     }
     else {
       console.log('ERROR');
@@ -27,7 +29,17 @@ function App() {
   }
 
 
-  //get data on page load
+  function formatDate(date) {
+    return new Date(date).toLocaleDateString();
+  }
+
+  function getCreatives(arr) {
+    let creatives = arr.filter(function (obj) {
+      return obj.type === 'creatives'
+    })
+    setCreatives(creatives);
+  }
+  
   useEffect(() => {
     getData();
   }, []);
@@ -38,6 +50,14 @@ function App() {
         <h1 className='heading'>{title}</h1>
         <p>Date: {date}</p>
         <p>{shortDescription}</p>
+        <ul>
+          <h2>Creatives</h2>
+              {
+                creatives.map((creative) => (
+                  <li key={creative.attributes.id}>{creative.attributes.name} ({creative.attributes.role})</li>
+                ))
+              }
+            </ul>
       </div>
     </div>
   );
